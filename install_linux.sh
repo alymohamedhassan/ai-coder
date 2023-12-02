5356f03b1d05a188408f997deea9bc8bfc26e8c7
Aly <alyhassan10@hotmail.com>
2023-12-02 02:33:52 +0200

    add latest version

--- install_linux.sh
+++ install_linux.sh
@@ -1,4 +1,4 @@
-curl -L -o aicoder https://github.com/alymohamedhassan/ai-coder/releases/download/Beta/aicoder
+curl -L -o aicoder https://github.com/alymohamedhassan/ai-coder/releases/download/v0.2/ai-coder
 
 chmod +x ./aicoder
 mv ./aicoder /usr/local/bin/aicoder
