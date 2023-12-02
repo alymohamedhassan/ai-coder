604c32d2ab792f9aebb79cf03b246e8d96a8ddf3
Aly <alyhassan10@hotmail.com>
2023-12-02 02:46:17 +0200

    fix install url

--- install_linux.sh
+++ install_linux.sh
@@ -1,4 +1,4 @@
-curl -L -o aicoder https://github.com/alymohamedhassan/ai-coder/releases/download/latest/ai-coder
+curl -L -o aicoder https://github.com/alymohamedhassan/ai-coder/releases/download/latest/aicoder
 
 chmod +x ./aicoder
 mv ./aicoder /usr/local/bin/aicoder
